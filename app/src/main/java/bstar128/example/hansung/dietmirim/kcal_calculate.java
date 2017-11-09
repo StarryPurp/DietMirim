a1493887814290da5e83caed9eb36ba769e76d64
StarryPurp <starbloom128@gmail.com>
2017-11-10 02:18:59 +0900

    #9 exercise layout make

--- app/src/main/java/bstar128/example/hansung/dietmirim/kcal_calculate.java
+++ app/src/main/java/bstar128/example/hansung/dietmirim/kcal_calculate.java
@@ -1,6 +1,7 @@
 package bstar128.example.hansung.dietmirim;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.Display;
@@ -19,11 +20,22 @@ import android.widget.Toast;
  * Created by HANSUNG on 2017-10-19.
  */
 public class kcal_calculate extends Activity{
+    Button test;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         //칼로리 계산
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kcal_calculate);
+
+        test=(Button)findViewById(R.id.test);
+
+       test.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent i=new Intent(getApplicationContext(),show_excercise.class);
+               startActivity(i);
+           }
+       });
 
 //        Toast tm=Toast.makeText(kcal_calculate.this,"기초 대사량: "+kcal,Toast.LENGTH_SHORT);
 //        Display dis=((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
---
+++ app/src/main/java/bstar128/example/hansung/dietmirim/show_excercise.java
@@ -0,0 +1,18 @@
+package bstar128.example.hansung.dietmirim;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by HANSUNG on 2017-11-10.
+ */
+
+public class show_excercise extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_excercise);
+    }
+}
