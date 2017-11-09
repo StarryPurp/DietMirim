f0c869bdfbb95e95334acedd4e934ca7226ff51f
xodms1701 <kimrrrrt123@naver.com>
2017-11-10 03:30:01 +0900

    firebase_add

--- app/src/main/java/bstar128/example/hansung/dietmirim/sign_in.java
+++ app/src/main/java/bstar128/example/hansung/dietmirim/sign_in.java
@@ -4,17 +4,29 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 /**
  * Created by HANSUNG on 2017-10-24.
  */
 
 public class sign_in extends Activity{
     //회원가입-> 성별, 이름, 이메일, 생년월일, 아이디, 비밀번호, 키, 몸무게
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
 
+        mAuth = FirebaseAuth.getInstance();
 
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
     }
 }
