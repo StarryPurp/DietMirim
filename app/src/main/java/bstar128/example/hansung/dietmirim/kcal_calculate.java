06e36fb844f57724e0e3f2ea50603aa0e55d91d9
StarryPurp <starbloom128@gmail.com>
2017-11-09 20:54:45 +0900

    #8 calculate edit complete

--- app/src/main/java/bstar128/example/hansung/dietmirim/kcal_calculate.java
+++ app/src/main/java/bstar128/example/hansung/dietmirim/kcal_calculate.java
@@ -19,46 +19,12 @@ import android.widget.Toast;
  * Created by HANSUNG on 2017-10-19.
  */
 public class kcal_calculate extends Activity{
-    EditText weight,height;
-    TextView result;
-    double bmi,kcal;
-    String w,h;
-    RadioGroup rdg;
-    RadioButton male,female;
-    Button cal;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         //칼로리 계산
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kcal_calculate);
-        height=(EditText)findViewById(R.id.kcal_height);
-        weight=(EditText)findViewById(R.id.kcal_weight);
-        rdg=(RadioGroup)findViewById(R.id.rdg);
-        male=(RadioButton)findViewById(R.id.male);
-        female=(RadioButton)findViewById(R.id.female);
-        cal=(Button)findViewById(R.id.calcul);
-        result=(TextView)findViewById(R.id.result);
 
-        w=weight.getText().toString();
-        h=height.getText().toString();
-
-        cal.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (rdg.getCheckedRadioButtonId()){
-                    case R.id.male:
-                        kcal=Double.parseDouble(w)*24.0;
-                        bmi=Double.parseDouble(w)/Math.pow(Double.parseDouble(h)/100,2);
-                        break;
-                    case R.id.female:
-                        kcal=Double.parseDouble(w)*24.0*0.9;
-                        bmi=Double.parseDouble(w)/Math.pow(Double.parseDouble(h)/100,2);
-                        break;
-                }
-                result.setText("기초 대사량: "+kcal+", BMI: "+bmi);
-                return false;
-            }
-        });
 //        Toast tm=Toast.makeText(kcal_calculate.this,"기초 대사량: "+kcal,Toast.LENGTH_SHORT);
 //        Display dis=((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
 //        int xoffset=(int)(Math.random()*dis.getWidth());
