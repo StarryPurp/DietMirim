8b7a2d9ca0ba759fc334a2e3440a7e99fb4dac13
StarryPurp <starbloom128@gmail.com>
2017-11-10 11:29:30 +0900

    #11 exercise java start

--- app/src/main/java/bstar128/example/hansung/dietmirim/show_excercise.java
+++ app/src/main/java/bstar128/example/hansung/dietmirim/show_excercise.java
@@ -3,12 +3,17 @@ package bstar128.example.hansung.dietmirim;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by HANSUNG on 2017-11-10.
  */
 
 public class show_excercise extends Activity {
+    TextView walk_kcal,walk_time,run_kcal,run_time,stairs_kcal,stairs_time,jumprope_kcal,jumprope_time,sqaut_kcal,sqaut_time,flank_kcal,flank_time,
+            updown_kcal,updown_time,kcal;
+    LinearLayout excercise;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
